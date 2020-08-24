0a90c633beebb86a7ee6cbe07d9f704de09b83d7
kirillkonik <64696994+kirillkonik@users.noreply.github.com>
2020-08-24 22:49:28 +0300

    delete derictories

--- conftest.py
+++ conftest.py
@@ -26,34 +26,12 @@ def browser(request):
         options = Options()
         options.add_experimental_option('prefs',
                                         {'intl.accept_languages': user_language})
-        print("\n\nStart chrome browser for test...")
         browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         fp = webdriver.FirefoxProfile()
         fp.set_preference("intl.accept_languages", user_language)
-        print("\n\nStart firefox browser for test...")
         browser = webdriver.Firefox(firefox_profile=fp)
     else:
         print("Browser <browser_name> still is not implemented")
     yield browser
-    print("\nQuit browser...")
     browser.quit()
-
-# @pytest.fixture(scope="function")
-# def browser():
-#    print("\nstart browser for test..")
-#    browser = webdriver.Chrome()
-#    browser.implicitly_wait(5)
-#    yield browser
-#    print("\nquit browser..")
-#    browser.quit()
-
-
-# options = Options()
-# options.add_experimental_option('prefs',\
-#                                {'intl.accept_languages': user_language})
-# browser = webdriver.Chrome(options=options)
-# Для Firefox объявление нужного языка будет выглядеть немного иначе:
-# fp = webdriver.FirefoxProfile()
-# fp.set_preference("intl.accept_languages", user_language)
-# browser = webdriver.Firefox(firefox_profile=fp)
--- test_product_page.py
+++ test_product_page.py
@@ -1,22 +1,19 @@
 from pages.product_page import ProductPage
-# from pages.base_page import BasePage
-# from pages.main_page import MainPage
 from pages.login_page import LoginPage
-# from pages.basket_page import BasketPage
 import pytest
 from pages.basket_page import BasketPage
 
 
-# @pytest.mark.parametrize('offer_num', ['0', '1', '2', '3', '4', '5', '6',
-#                                        pytest.param('7', marks=pytest.mark.xfail), '8', '9'])
-# def test_guest_can_add_product_to_basket(browser, offer_num):
-#     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{offer_num}"
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.click_add_to_cart_button()
-#     page.solve_quiz_and_get_code()
-#     page.should_be_message_about_adding()
-#     page.should_be_total_price()
+@pytest.mark.parametrize('offer_num', ['0', '1', '2', '3', '4', '5', '6',
+                                       pytest.param('7', marks=pytest.mark.xfail), '8', '9'])
+def test_guest_can_add_product_to_basket(browser, offer_num):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{offer_num}"
+    page = ProductPage(browser, link)
+    page.open()
+    page.click_add_to_cart_button()
+    page.solve_quiz_and_get_code()
+    page.should_be_message_about_adding()
+    page.should_be_total_price()
 
 
 @pytest.mark.xfail
